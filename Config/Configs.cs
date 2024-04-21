bc4a04ebaf3b37b53e509c2cbe955bc3312ad02e
Gold KingZ <48490385+oqyh@users.noreply.github.com>
2024-04-21 13:53:50 +0400

    1.0.9

--- Config/Configs.cs
+++ Config/Configs.cs
@@ -12,9 +12,11 @@ namespace Vote_GoldKingZ.Config
         
         private static readonly string ConfigDirectoryName = "config";
         private static readonly string ConfigFileName = "config.json";
-        private static readonly string jsonFilePath = "GameMode.json";
+        private static readonly string jsonFilePath = "VoteGameMode.json";
+        private static readonly string jsonFilePath2 = "VoteMap.json";
         private static string? _configFilePath;
         private static string? _jsonFilePath;
+        private static string? _jsonFilePath2;
         private static ConfigData? _configData;
 
         private static readonly JsonSerializerOptions SerializationOptions = new()
@@ -69,6 +71,9 @@ namespace Vote_GoldKingZ.Config
 
             _jsonFilePath = Path.Combine(configFileDirectory, jsonFilePath);
             Helper.CreateDefaultWeaponsJson(_jsonFilePath);
+            
+            _jsonFilePath2 = Path.Combine(configFileDirectory, jsonFilePath2);
+            Helper.CreateDefaultWeaponsJson2(_jsonFilePath2);
 
             _configFilePath = Path.Combine(configFileDirectory, ConfigFileName);
             if (File.Exists(_configFilePath))
@@ -102,6 +107,9 @@ namespace Vote_GoldKingZ.Config
 
         public class ConfigData
         {
+            public string VoteAdmin_CommandsInGame { get; set; }
+            public string VoteAdmin_Groups { get; set; }
+            public string empty { get; set; }
             private int _voteKickMode;
             public int VoteKick_Mode
             {
@@ -124,6 +132,7 @@ namespace Vote_GoldKingZ.Config
                 }
             }
             public int VoteKick_TimeInMins { get; set; }
+            public bool VoteKick_ChangeTimeInMinsToDays { get; set; }
             public int VoteKick_StartOnMinimumOfXPlayers { get; set; }
             public bool VoteKick_AllowKickedPlayersToJoinOnMapChange { get; set; }
             public bool VoteKick_TeamOnly { get; set; }
@@ -138,7 +147,7 @@ namespace Vote_GoldKingZ.Config
             public string VoteKick_CommandsOnHalfVoteRefuse { get; set; }
             public string VoteKick_ImmunityGroups { get; set; }
             public string VoteKick_DisableItOnJoinTheseGroups { get; set; }
-            public string empty { get; set; }
+            public string empty2 { get; set; }
 
 
             private int _voteBannedMode;
@@ -161,7 +170,8 @@ namespace Vote_GoldKingZ.Config
                     }
                 }
             }
-            public int VoteBanned_TimeInDays { get; set; }
+            public int VoteBanned_TimeInMins { get; set; }
+            public bool VoteBanned_ChangeTimeInMinsToDays { get; set; }
             public int VoteBanned_StartOnMinimumOfXPlayers { get; set; }
             public bool VoteBanned_TeamOnly { get; set; }
             public float VoteBanned_Percentage { get; set; }
@@ -175,10 +185,11 @@ namespace Vote_GoldKingZ.Config
             public string VoteBanned_CommandsOnHalfVoteRefuse { get; set; }
             public string VoteBanned_ImmunityGroups { get; set; }
             public string VoteBanned_DisableItOnJoinTheseGroups { get; set; }
-            public string empty2 { get; set; }
+            public string empty3 { get; set; }
 
             public bool VoteMute { get; set; }
             public int VoteMute_TimeInMins { get; set; }
+            public bool VoteMute_ChangeTimeInMinsToDays { get; set; }
             public int VoteMute_StartOnMinimumOfXPlayers { get; set; }
             public bool VoteMute_RemoveMutedPlayersOnMapChange { get; set; }
             public bool VoteMute_TeamOnly { get; set; }
@@ -194,9 +205,10 @@ namespace Vote_GoldKingZ.Config
             public string VoteMute_DisableItOnJoinTheseGroups { get; set; }
 
 
-            public string empty3 { get; set; }
+            public string empty4 { get; set; }
             public bool VoteGag { get; set; }
             public int VoteGag_TimeInMins { get; set; }
+            public bool VoteGag_ChangeTimeInMinsToDays { get; set; }
             public int VoteGag_StartOnMinimumOfXPlayers { get; set; }
             public bool VoteGag_RemoveGagedPlayersOnMapChange { get; set; }
             public bool VoteGag_TeamOnly { get; set; }
@@ -211,9 +223,10 @@ namespace Vote_GoldKingZ.Config
             public string VoteGag_LetTheseAllowedForGagedPlayers { get; set; }
             public string VoteGag_ImmunityGroups { get; set; }
             public string VoteGag_DisableItOnJoinTheseGroups { get; set; }
-            public string empty4 { get; set; }
+            public string empty5 { get; set; }
             public bool VoteSilent { get; set; }
             public int VoteSilent_TimeInMins { get; set; }
+            public bool VoteSilent_ChangeTimeInMinsToDays { get; set; }
             public int VoteSilent_StartOnMinimumOfXPlayers { get; set; }
             public bool VoteSilent_RemoveSilentedPlayersOnMapChange { get; set; }
             public bool VoteSilent_TeamOnly { get; set; }
@@ -228,7 +241,7 @@ namespace Vote_GoldKingZ.Config
             public string VoteSilent_LetTheseAllowedForSilentedPlayers { get; set; }
             public string VoteSilent_ImmunityGroups { get; set; }
             public string VoteSilent_DisableItOnJoinTheseGroups { get; set; }
-            public string empty5 { get; set; }
+            public string empty6 { get; set; }
             public bool VoteGameMode { get; set; }
             public int VoteGameMode_StartOnMinimumOfXPlayers { get; set; }
             public int VoteGameMode_Percentage { get; set; }
@@ -238,10 +251,21 @@ namespace Vote_GoldKingZ.Config
             public string VoteGameMode_CommandsOnHalfVoteAccept { get; set; }
             public string VoteGameMode_CommandsOnHalfVoteRefuse { get; set; }
             public string VoteGameMode_DisableItOnJoinTheseGroups { get; set; }
-            public string empty6 { get; set; }
+            public string empty7 { get; set; }
+            public bool VoteMap { get; set; }
+            public int VoteMap_StartOnMinimumOfXPlayers { get; set; }
+            public int VoteMap_Percentage { get; set; }
+            public bool VoteMap_CenterMessageAnnouncementOnHalfVotes { get; set; }
+            public float VoteMap_CenterMessageAnnouncementTimer { get; set; }
+            public string VoteMap_CommandsToVote { get; set; }
+            public string VoteMap_CommandsOnHalfVoteAccept { get; set; }
+            public string VoteMap_CommandsOnHalfVoteRefuse { get; set; }
+            public string VoteMap_DisableItOnJoinTheseGroups { get; set; }            
+            public string empty8 { get; set; }
             public bool Log_SendLogToText { get; set; }
             public string Log_TextMessageFormat { get; set; }
             public string Log_GameModeFormat { get; set; }
+            public string Log_MapFormat { get; set; }
             public int Log_AutoDeleteLogsMoreThanXdaysOld { get; set; }
             private int _Log_SendLogToDiscordOnMode;
             public int Log_SendLogToDiscordOnMode
@@ -279,14 +303,19 @@ namespace Vote_GoldKingZ.Config
             public string Log_DiscordWebHookURL { get; set; }
             public string Log_DiscordMessageFormat { get; set; }
             public string Log_DiscordGameModeFormat { get; set; }
+            public string Log_DiscordMapFormat { get; set; }
             public string Log_DiscordUsersWithNoAvatarImage { get; set; }
-            public string empty7 { get; set; }
+            public string empty9 { get; set; }
             public string Information_For_You_Dont_Delete_it { get; set; }
             
             public ConfigData()
             {
+                VoteAdmin_CommandsInGame = "!votesadmin,!voteadmin";
+                VoteAdmin_Groups = "@css/root,@css/admin";
+                empty = "-----------------------------------------------------------------------------------";
                 VoteKick_Mode = 2;
                 VoteKick_TimeInMins = 5;
+                VoteKick_ChangeTimeInMinsToDays = false;
                 VoteKick_StartOnMinimumOfXPlayers  = 5;
                 VoteKick_AllowKickedPlayersToJoinOnMapChange = false;
                 VoteKick_TeamOnly = false;
@@ -301,9 +330,10 @@ namespace Vote_GoldKingZ.Config
                 VoteKick_CommandsOnHalfVoteRefuse = "!no,no,!n,n";
                 VoteKick_ImmunityGroups = "@css/root,@css/admin,@css/vip,#css/admin,#css/vip";
                 VoteKick_DisableItOnJoinTheseGroups = "";
-                empty = "-----------------------------------------------------------------------------------";
+                empty2 = "-----------------------------------------------------------------------------------";
                 VoteBanned_Mode = 0;
-                VoteBanned_TimeInDays = 1;
+                VoteBanned_TimeInMins = 1;
+                VoteBanned_ChangeTimeInMinsToDays = true;
                 VoteBanned_StartOnMinimumOfXPlayers  = 8;
                 VoteBanned_TeamOnly = false;
                 VoteBanned_Percentage = 80;
@@ -317,9 +347,10 @@ namespace Vote_GoldKingZ.Config
                 VoteBanned_CommandsOnHalfVoteRefuse = "!no,no,!n,n";
                 VoteBanned_ImmunityGroups = "@css/root,@css/admin,@css/vip,#css/admin,#css/vip";
                 VoteBanned_DisableItOnJoinTheseGroups = "";
-                empty2 = "-----------------------------------------------------------------------------------";
+                empty3 = "-----------------------------------------------------------------------------------";
                 VoteMute = false;
                 VoteMute_TimeInMins = 5;
+                VoteMute_ChangeTimeInMinsToDays = false;
                 VoteMute_StartOnMinimumOfXPlayers  = 5;
                 VoteMute_RemoveMutedPlayersOnMapChange = false;
                 VoteMute_TeamOnly = false;
@@ -333,9 +364,10 @@ namespace Vote_GoldKingZ.Config
                 VoteMute_CommandsOnHalfVoteRefuse = "!no,no,!n,n";
                 VoteMute_ImmunityGroups = "@css/root,@css/admin,@css/vip,#css/admin,#css/vip";
                 VoteMute_DisableItOnJoinTheseGroups = "";
-                empty3 = "-----------------------------------------------------------------------------------";
+                empty4 = "-----------------------------------------------------------------------------------";
                 VoteGag = false;
                 VoteGag_TimeInMins = 5;
+                VoteGag_ChangeTimeInMinsToDays = false;
                 VoteGag_StartOnMinimumOfXPlayers  = 5;
                 VoteGag_RemoveGagedPlayersOnMapChange = false;
                 VoteGag_TeamOnly = false;
@@ -350,9 +382,10 @@ namespace Vote_GoldKingZ.Config
                 VoteGag_LetTheseAllowedForGagedPlayers = "!,.,@";
                 VoteGag_ImmunityGroups = "@css/root,@css/admin,@css/vip,#css/admin,#css/vip";
                 VoteGag_DisableItOnJoinTheseGroups = "";
-                empty4 = "-----------------------------------------------------------------------------------";
+                empty5 = "-----------------------------------------------------------------------------------";
                 VoteSilent = false;
                 VoteSilent_TimeInMins = 5;
+                VoteSilent_ChangeTimeInMinsToDays = false;
                 VoteSilent_StartOnMinimumOfXPlayers  = 5;
                 VoteSilent_RemoveSilentedPlayersOnMapChange = false;
                 VoteSilent_TeamOnly = false;
@@ -367,28 +400,40 @@ namespace Vote_GoldKingZ.Config
                 VoteSilent_LetTheseAllowedForSilentedPlayers = "!,.,@";
                 VoteSilent_ImmunityGroups = "@css/root,@css/admin,@css/vip,#css/admin,#css/vip";
                 VoteSilent_DisableItOnJoinTheseGroups = "";
-                empty5 = "-----------------------------------------------------------------------------------";
+                empty6 = "-----------------------------------------------------------------------------------";
                 VoteGameMode = false;
                 VoteGameMode_StartOnMinimumOfXPlayers  = 5;
                 VoteGameMode_Percentage = 60;
                 VoteGameMode_CenterMessageAnnouncementOnHalfVotes = false;
                 VoteGameMode_CenterMessageAnnouncementTimer = 25;
-                VoteGameMode_CommandsToVote = "!votemode,!votem";
+                VoteGameMode_CommandsToVote = "!votemodes,!votemode";
                 VoteGameMode_CommandsOnHalfVoteAccept = "!yes,yes,!y,y";
                 VoteGameMode_CommandsOnHalfVoteRefuse = "!no,no,!n,n";
                 VoteGameMode_DisableItOnJoinTheseGroups = "";
-                empty6 = "-----------------------------------------------------------------------------------";
+                empty7 = "-----------------------------------------------------------------------------------";
+                VoteMap = false;
+                VoteMap_StartOnMinimumOfXPlayers  = 5;
+                VoteMap_Percentage = 60;
+                VoteMap_CenterMessageAnnouncementOnHalfVotes = false;
+                VoteMap_CenterMessageAnnouncementTimer = 25;
+                VoteMap_CommandsToVote = "!votemaps,!votemap";
+                VoteMap_CommandsOnHalfVoteAccept = "!yes,yes,!y,y";
+                VoteMap_CommandsOnHalfVoteRefuse = "!no,no,!n,n";
+                VoteMap_DisableItOnJoinTheseGroups = "";                
+                empty8 = "-----------------------------------------------------------------------------------";
                 Log_SendLogToText = false;
                 Log_TextMessageFormat = "[{DATE} - {TIME}] {PLAYERNAME} Has Been ({REASON})  [SteamID: {STEAMID} - Ip: {IP}]";
                 Log_GameModeFormat = "[{DATE} - {TIME}] Vote Game Mode Choosed To Change To ({GAMEMODE})";
+                Log_MapFormat = "[{DATE} - {TIME}] Vote Map Choosed To Change To ({MAP})";
                 Log_AutoDeleteLogsMoreThanXdaysOld = 0;
                 Log_SendLogToDiscordOnMode = 0;
                 Log_DiscordSideColor = "00FFFF";
                 Log_DiscordWebHookURL = "https://discord.com/api/webhooks/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
                 Log_DiscordMessageFormat = "[{DATE} - {TIME}] {PLAYERNAME} Has Been ({REASON})  [SteamID: {STEAMID} - Ip: {IP}]";
                 Log_DiscordGameModeFormat = "[{DATE} - {TIME}] Vote Game Mode Choosed To Change To ({GAMEMODE})";
+                Log_DiscordMapFormat = "[{DATE} - {TIME}] Vote Map Choosed To Change To ({MAP})";
                 Log_DiscordUsersWithNoAvatarImage = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b5/b5bd56c1aa4644a474a2e4972be27ef9e82e517e_full.jpg";
-                empty7 = "-----------------------------------------------------------------------------------";
+                empty9 = "-----------------------------------------------------------------------------------";
                 Information_For_You_Dont_Delete_it = " Vist  [https://github.com/oqyh/cs2-Vote-GoldKingZ/tree/main?tab=readme-ov-file#-configuration-] To Understand All Above";
             }
         }
